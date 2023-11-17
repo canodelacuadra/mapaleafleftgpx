4aaec54ac1670269ae46c9bb6eccdc653e8ca920
canodelacuadra <canodelacuadra@gmail.com>
2023-11-17 12:21:44 +0100

    agrego logo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([40.03714891012755, -3.608062792760403], 17);
+var map = L.map('map').setView([40.03931, -3.60993], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: 'Tandem Aranjuez, 2023-2024'
 }).addTo(map);
-
+//map.locate({setView: true, maxZoom: 16});
 L.marker([40.03698, -3.60759]).addTo(map)
     .bindPopup('<a href="fuentedelaboticaria.html">Fuente de la Boticaria</a>')
     .openPopup();
@@ -14,3 +14,7 @@ L.marker([40.03806, -3.60925]).addTo(map)
 L.marker([40.03687, -3.60803]).addTo(map)
     .bindPopup('<a href="puente_reina.html">Puente de  la Reina</a>')
     .openPopup();
+    // insertando una imange
+    var imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Logo_de_Patrimonio_Nacional.svg/1200px-Logo_de_Patrimonio_Nacional.svg.png',
+    imageBounds = [[40.04259, -3.61225], [40.04146, -3.60919]];
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
