852c9b6ce7cbf2528d04a77dddc0289178e49674
canodelacuadra <canodelacuadra@gmail.com>
2023-11-17 21:14:05 +0100

    enlaces externos a marcadores

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var map = L.map('map').setView([40.03931, -3.60993], 17);
+var map = L.map('map').setView([40.03850, -3.60993], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: 'Tandem Aranjuez, 2023-2024'
 }).addTo(map);
 map.locate({setView: true, maxZoom: 16});
-L.marker([40.03698, -3.60759]).addTo(map)
+let markerBoticaria = L.marker([40.03698, -3.60759]).addTo(map)
     .bindPopup('<a href="fuentedelaboticaria.html">Fuente de la Boticaria</a>')
     .openPopup();
-L.marker([40.03846, -3.60797]).addTo(map)
+let markerReyes =L.marker([40.03846, -3.60797]).addTo(map)
     .bindPopup('<a href="salonreyescatolicos.html">Salón de los Reyes Católicos</a>');
-L.marker([40.03806, -3.60925]).addTo(map)
+let markerEspina=L.marker([40.03806, -3.60925]).addTo(map)
     .bindPopup('<a href="fuentenino.html">Fuente del Niño de la Espina</a>');
 L.marker([40.03687, -3.60803]).addTo(map)
-    .bindPopup('<a href="puente_reina.html">Puente de  la Reina</a>')
+    .bindPopup('<a href="puente_reina.html">Acceso desde el jardím la Reina</a>')
     .openPopup();
     // insertando una imange
     var imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Logo_de_Patrimonio_Nacional.svg/1200px-Logo_de_Patrimonio_Nacional.svg.png',
