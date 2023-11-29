5f1b60d3e039a11486cb8342bc696294d5d78209
canodelacuadra <canodelacuadra@gmail.com>
2023-11-29 15:56:05 +0100

    actualizo

--- leaflet-geoloca.js
+++ leaflet-geoloca.js
@@ -1,38 +1,30 @@
 let btnGeo = document.getElementById('btn-geo');
-function geolocalizacion() {
-    if(btnGeo.classList.contains( 'active' )){
-        btnGeo.textContent='Activar Geolocalización';
-        btnGeo.classList.remove('active');
-         // geolocalización anulada
-    map.stopLocate({setView: false, maxzoom: 16});
-
-    }else{
-
-    // geolocalización
-    map.locate({ setView: true, maxZoom: 16 });
-    //añadimos un circulo  si funciona la geolocolización
-    function onLocationFound(e) {
-    var radius = e.accuracy;
-
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("Está usted a  " + radius + " metros de este punto").openPopup();
-
-    L.circle(e.latlng, radius).addTo(map);
-   
-    }
-    // Si la geolocalización  falla
-    map.on('locationfound', onLocationFound);
-    function onLocationError(e) {
-    alert(e.message);
-    }
-    // mostramos el error
-    map.on('locationerror', onLocationError);
-    // añadimos al boton la clase active para despues poder desactivarlo
-    btnGeo.classList.add('active'); 
-      btnGeo.textContent='Desactivar Geolocalización';
-    } 
+function geolocalizacion(){
+     // geolocalización
+     map.locate({ setView: true, maxZoom: 16 });
+     //creamos una funcioon si la geolocalización esta activa
+     function onLocationFound(e) {
+     var radius = e.accuracy;
+         // le añañadimus un marcador que nos muestra los metros a los que estamos del mapa
+     var markerLocation= L.marker(e.latlng).addTo(map)
+      .bindPopup("Está usted a  " + radius + " metros de este punto").openPopup();
+ 
+    var markerCirculo =  L.circle(e.latlng, radius).addTo(map);
+    
+     }
+ 
+     map.on('locationfound', onLocationFound);
+         // Si la geolocalización  falla
+     function onLocationError(e) {
+     alert(e.message);
+     }
+     // mostramos el error
+     map.on('locationerror', onLocationError);
+     // añadimos al boton la clase active para despues poder desactivarlo
+     btnGeo.classList.add('active'); 
+       btnGeo.textContent='Desactivar Geolocalización';
 }
+    
 
-btnGeo.addEventListener('click', function () {
-    geolocalizacion();
-})
+
+
